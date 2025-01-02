f1863257778f8b0280695cdfa446bde60f78c712
brooksbrill <babrill06@gmail.com>
2025-01-01 23:02:37 -0600

    made image generator using pollinations.ai api

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { generate } from "random-words";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -9,18 +9,22 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const [numclicks, setNumclicks] = useState(0)
+  const [numclicks, setNumclicks] = useState(0);
+  const numclicks3 = Math.floor(numclicks / 3);
+
+  const [imgurl, setImgurl] = useState(`https://pollinations.ai/prompt/${generate()}`)
+  useEffect(() => { setImgurl(`https://pollinations.ai/prompt/${generate()}`) }, [numclicks3])
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center" onClick={() => setNumclicks(numclicks + 1)}>
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Click image for random image
+            Click anywhere three times for random image
           </h1>
           <div className="h-[144px] w-[434px]">
             <img
-              onClick={() => setNumclicks(numclicks + 1)}
-              src={`https://loremflickr.com/200/200?random=${Math.floor(numclicks / 3)}`}
+
+              src={imgurl}
               alt="Remix"
               className="block w-full"
             />
